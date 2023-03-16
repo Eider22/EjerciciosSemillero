9cb3559858d0197b5b1d783e1402fc80b6339d95
Eider22 <eidercardona22@gmail.com>
2023-03-15 20:43:39 -0500

    Se ca,bia la forma en la que se de formato a la fecha de venta. En lugar de usar un atributo static se usa el getter

--- src/task/one/Factura.java
+++ src/task/one/Factura.java
@@ -8,7 +8,6 @@ public class Factura extends Precio {
 	public Date fecha;
 	private Persona emisor;
 	private Persona cliente;
-	static private SimpleDateFormat formattter = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Factura(Persona emisor, Persona cliente) {
 		this.emisor = emisor;
@@ -18,8 +17,35 @@ public class Factura extends Precio {
 
 	public void imprimirFactura() {
 		JOptionPane.showMessageDialog(null,
-				"Fecha: " + this.formattter.format(this.fecha) + "\n" + "Emisor: " + this.emisor.getPrimerNombre() + " "
-						+ this.emisor.getPrimerApellido() + "\n" + "Cliente: " + this.cliente.getPrimerNombre() + " "
-						+ this.cliente.getPrimerApellido());
+				"Fecha: " + this.getFecha() + "\n" + "Emisor: " + this.getEmisor().getPrimerNombre() + " "
+						+ this.getEmisor().getPrimerApellido() + "\n" + "Cliente: "
+						+ this.getCliente().getPrimerNombre() + " " + this.getCliente().getPrimerApellido());
 	}
+
+	public String getFecha() {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		String formatedDate = formatter.format(this.fecha);
+		return formatedDate;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Persona getEmisor() {
+		return emisor;
+	}
+
+	public void setEmisor(Persona emisor) {
+		this.emisor = emisor;
+	}
+
+	public Persona getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Persona cliente) {
+		this.cliente = cliente;
+	}
+
 }
